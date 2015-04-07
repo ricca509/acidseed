74994f08ff5394159965f51b6114cc5f271a588f
Riccardo Coppola <coppola.riccardo@gmail.com>
2015-04-08 00:32:53 +0200

    - Proxies the request's headers instead of creating a new one - Refactors functions in helpers (temp)

--- server.js
+++ server.js
@@ -1,48 +1,40 @@
-var express     = require('express'),
+var express       = require('express'),
     app           = express(),
     bodyParser    = require('body-parser'),
     redis         = require("redis"),
     sha256        = require('sha256'),
+    colors        = require('colors'),
+    _             = require('underscore'),
     http          = require('http');
 
+var rtg   = require("url").parse('redis://redistogo:3b3bd4d79bc23c76fb5b3f180f7681ae@soapfish.redistogo.com:9335/');
+var redisClient = redis.createClient(rtg.port, rtg.hostname);
+redisClient.auth(rtg.auth.split(":")[1]);
 
-// var rtg   = require("url").parse('redis://redistogo:3b3bd4d79bc23c76fb5b3f180f7681ae@soapfish.redistogo.com:9335/');
-// var redisClient = redis.createClient(rtg.port, rtg.hostname);
-//
-// redisClient.auth(rtg.auth.split(":")[1]);
-var redisClient = redis.createClient();
+//var redisClient = redis.createClient();
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
+// Parses body as JSON
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3131;        // set our port
+var port = process.env.PORT || 3131;
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router();
 
 router.get('/ping', function (req, res) {
     res.json({ message: 'hooray! welcome to lastminute.com\'s acidseed!' });
 });
 
 router.post('/request', function (req, res) {
-    //res.json(JSON.parse(data));
-    // Get the request body
-    var body = req.body,
-        cacheKeyPrefix = 'cache.';
-    // Create a string representation of the body
-    var key = [cacheKeyPrefix, sha256(body)].join('');
+    // Create a string representation of the request for the key
+    var key = getKey(req);
+
     // Check the redis server for the key
     redisClient.get(key, function(err, reply) {
         // If it exists
         if (!reply) {
-          console.log('No Redis cache for', key);
-          // - Forward the post request to the endpoint passed in the querystring
-          // - Save the data to redis using the generated key
-          // - Return the data back
+          console.log(('No Redis cache for ' + key).red);
           var url = req.query.requestedUrl.match(/http:\/\/([^\/]+)(.*)/);
 
           var options = {
@@ -50,38 +42,23 @@ router.post('/request', function (req, res) {
               path: url[2],
               port: 80,
               method: 'POST',
-              headers: {
-                  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-                  'Content-Type': 'application/json'
-              }
+              headers: _.pick(req.headers, 'user-agent', 'content-type')
           };
 
-          var request = http.request(options, function (response) {
-            var data = '';
-            console.log('STATUS: ' + response.statusCode);
+          console.log('Damn! Calling the API'.green);
+          proxyRequest(options, req, function onData (data) {
+            var parsedApiResp = JSON.parse(data);
 
-            response.setEncoding('utf8');
-            var apiResp = '';
-            response.on('data', function(d) {
-              apiResp += d;
-            });
+            // Save the data to redis using the generated key
+            console.log('Saving to Redis'.green);
+            redisClient.set(key, data, redis.print);
 
-            response.on('end', function() {
-                var parsedApiResp = JSON.parse(apiResp);
-
-                redisClient.set(key, apiResp, redis.print);
-                console.log('Response from API');
-                res.json(parsedApiResp);
-            });
+            // Return the data back
+            res.json(parsedApiResp);
+          }, function onError () {
+            console.log(('problem with request: ' + e.message).red);
           });
 
-          request.on('error', function(e) {
-            console.log('problem with request: ' + e.message);
-          });
-
-          // write data to request body
-          request.write(body);
-          request.end();
         } else {
             console.log('Response from Redis for', key);
             res.json(JSON.parse(reply));
@@ -90,18 +67,63 @@ router.post('/request', function (req, res) {
 
 });
 
-router.get('/request', function (req, res) {
-    var data = req.query;
-    var key = 'cache.' + data.requestedUrl
-    redisClient.set(key, 'something', redis.print);
-    res.json(data);
-})
+// router.get('/request', function (req, res) {
+//     var data = req.query;
+//     var key = 'cache.' + data.requestedUrl;
+//     redisClient.set(key, 'something', redis.print);
+//     res.json(data);
+// });
+
+// TEMP HELPERS
+
+/**
+ *
+ */
+var getKey = function (request) {
+  var key, hashValue,
+      cacheKeyPrefix = 'cache.';
+
+  var stringifiedBody = request.body ? JSON.stringify(request.body) : null;
+
+  hashValue = stringifiedBody ? stringifiedBody : request.query;
+
+  return [cacheKeyPrefix, sha256(hashValue)].join('');
+};
+
+/**
+ *
+ */
+var proxyRequest = function (options, originalReq, onData, onError) {
+  // Forward the post request to the endpoint passed in the querystring
+  var request = http.request(options, function (response) {
+    var apiResp = '';
+    console.log('STATUS: ' + response.statusCode);
+
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      apiResp += data;
+    });
+
+    response.on('end', function () {
+      onData(apiResp);
+    });
+  });
+
+  request.on('error', onError);
+
+  if (originalReq.body) {
+    // write data to request body
+    request.write(JSON.stringify(originalReq.body));
+  }
+  request.end();
+};
+
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+// All of our routes will be prefixed with /api
 app.use('/cache', router);
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log(('acidseed: Magic happens on port ' + port).green);
